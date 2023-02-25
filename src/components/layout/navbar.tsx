999b20d6a00262e223e7e8ef753fbc01353f50ad
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-02-24 17:16:05 -0700

    update site logo and favicon

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,29 +1,26 @@
-import { Button, Navbar } from "flowbite-react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import React from 'react';
+import { Button, Navbar } from 'flowbite-react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const navbar: React.FC = () => {
   const { data: sessionData } = useSession();
 
   return (
     <Navbar fluid={true} rounded={true}>
-      <Navbar.Brand href="https://500jobs.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      <Navbar.Brand href='/'>
+        <img src='/briefcase.png' className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
+        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
           500Jobs
         </span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
+      <div className='flex md:order-2'>
         <Button
-          gradientDuoTone="pinkToOrange"
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
+          gradientDuoTone='pinkToOrange'
+          onClick={sessionData ? () => signOut() : () => signIn()}
         >
-          {sessionData ? "Sign out" : "Sign in"}
+          {sessionData ? 'Sign out' : 'Sign in'}
         </Button>
-        <Navbar.Toggle />
+        {/* <Navbar.Toggle /> */}
       </div>
     </Navbar>
   );
