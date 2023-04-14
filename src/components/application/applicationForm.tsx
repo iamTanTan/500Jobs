d637a0eda7c70e011ba6c249001e2bc3c15c7905
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-04-13 19:50:53 -0700

    update app form to show company name

--- src/components/application/applicationForm.tsx
+++ src/components/application/applicationForm.tsx
@@ -15,24 +15,42 @@ interface ApplicationForm {
 
 const ApplicationForm = () => {
   const { mutate } = api.application.createApplication.useMutation();
+  const { data, isLoading, error } = api.company.getAll.useQuery(); // replace with zustand or atom
   const router = useRouter();
 
   const initialValues: ApplicationForm = {
     status: 'Applied',
     position: '',
     location: '',
-    date: new Date().toISOString().split('T')[0],
+    date: new Date().toISOString().slice(0, 10),
     notes: '',
     companyId: '1',
   };
 
+  if (isLoading) {
+    return (
+      <div className='grid h-full w-full grid-cols-6 gap-4'>
+        <div className='col-span-4 col-start-2 '></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>{'An Error has occurred: ' + error.message}</div>;
+  }
+
+  if (!data) {
+    return (
+      <div className='grid grid-cols-6 gap-4'>
+        <div className='col-span-4 col-start-2 h-full w-full'></div>
+      </div>
+    );
+  }
+
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={async (
-        values: ApplicationForm,
-        { setSubmitting }: FormikHelpers<ApplicationForm>,
-      ) => {
+      onSubmit={(values: ApplicationForm, { setSubmitting }: FormikHelpers<ApplicationForm>) => {
         mutate({
           ...values,
           date: new Date(values.date).toISOString(),
@@ -44,14 +62,14 @@ const ApplicationForm = () => {
     >
       <div className='grid grid-cols-6 gap-4'>
         <div className='col-span-4 col-start-2 w-full'>
-          <Form className='mb-4 rounded bg-white px-8 pt-6 pb-8 shadow-md'>
+          <Form className='mb-4 rounded px-8 pt-6 pb-8 shadow-md dark:bg-midnight_light'>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='status'>
+              <label className='mb-2 block text-sm font-bold' htmlFor='status'>
                 Application Status
               </label>
             </div>
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='status'
               required={true}
               as='select'
@@ -63,61 +81,66 @@ const ApplicationForm = () => {
               <option>Rejected</option>
             </Field>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='position'>
+              <label className='mb-2 block text-sm font-bold ' htmlFor='position'>
                 Position
               </label>
             </div>
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='position'
               type='text'
               required={true}
             ></Field>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='location'>
+              <label className='mb-2 block text-sm font-bold ' htmlFor='location'>
                 Location
               </label>
             </div>
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='location'
               type='text'
               required={true}
             ></Field>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='Applied Date'>
+              <label className='mb-2 block text-sm font-bold ' htmlFor='Applied Date'>
                 Applied Date
               </label>
             </div>
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='date'
               type='date'
               required={true}
             ></Field>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='Notes'>
+              <label className='mb-2 block text-sm font-bold ' htmlFor='Notes'>
                 Notes
               </label>
             </div>
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='notes'
               as='textarea'
             ></Field>
             <div className='mb-2 block'>
-              <label className='mb-2 block text-sm font-bold text-gray-700' htmlFor='companyId'>
+              <label className='mb-2 block text-sm font-bold ' htmlFor='companyId'>
                 Company
               </label>
             </div>
+
             <Field
-              className='focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none'
+              className='focus:shadow-outline my-1 w-full appearance-none rounded border py-2 px-3 leading-tight shadow-sm dark:border-black dark:bg-slate-600'
               name='companyId'
               as='select'
               required={true}
             >
-              {[...Array(500).keys()].map((element) => {
-                return <option key={element}>{element + 1}</option>;
+              {data.map((element) => {
+                return (
+                  <option key={element.id} value={element.id}>
+                    {element.name}
+                  </option>
+                );
               })}
             </Field>
 
