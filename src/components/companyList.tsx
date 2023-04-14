782fb909791a4fa41b2096cdcbc08115f8aa220d
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-04-13 19:51:44 -0700

    update company list name style

--- src/components/companyList.tsx
+++ src/components/companyList.tsx
@@ -10,6 +10,9 @@ const CompanyList: React.FC<CompanyListProps> = ({ companies }) => {
   return (
     <div className='grid grid-cols-6 gap-4'>
       <div className='col-span-4 col-start-2'>
+        <h1 className='my-4 text-center text-3xl font-bold dark:text-white dark:underline'>
+          500 List
+        </h1>
         {companies.map((c) => (
           <CompanyCard key={c.id} company={c} />
         ))}
