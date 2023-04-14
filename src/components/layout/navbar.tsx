2d8752a97a5e73975cdd7744dc6fb54aacbf2a64
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-04-13 19:34:38 -0700

    update navbar for mobile and theme

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,39 +1,58 @@
 import React from 'react';
 import { Button, Navbar } from 'flowbite-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 const navbar: React.FC = () => {
   const { data: sessionData } = useSession();
-  const router = useRouter();
-  console.log('path', router.asPath);
 
   return (
-    <Navbar fluid={true} rounded={true}>
+    <Navbar className='bg-primary-50 dark:!bg-midnight' fluid={true} rounded={false}>
       <Navbar.Brand href='/'>
         <img src='/briefcase.png' className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
         <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
           500Jobs
         </span>
       </Navbar.Brand>
-      {sessionData && router.asPath != '/application' ? (
-        <div className='ml-auto mr-2'>
-          <Button gradientDuoTone='pinkToOrange' onClick={() => router.push('/application')}>
-            + Application
-          </Button>
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className='flex md:order-2'>
+
+      <div className='hidden md:order-2 md:block'>
         <Button
           gradientDuoTone='pinkToOrange'
           onClick={sessionData ? () => signOut() : () => signIn()}
         >
           {sessionData ? 'Sign out' : 'Sign in'}
         </Button>
-        {/* <Navbar.Toggle /> */}
       </div>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link
+          className='!text-black underline-offset-4 hover:underline dark:!text-slate-300'
+          href='/'
+        >
+          Home
+        </Navbar.Link>
+        {sessionData && (
+          <Navbar.Link
+            className='!text-black underline-offset-4 hover:underline dark:!text-slate-300'
+            href='/my_apps'
+          >
+            My Apps
+          </Navbar.Link>
+        )}
+        {sessionData && (
+          <Navbar.Link
+            className='!text-black underline-offset-4 hover:underline dark:!text-slate-300'
+            href='/application'
+          >
+            Create Application
+          </Navbar.Link>
+        )}
+        <Navbar.Link
+          className='block md:hidden'
+          onClick={sessionData ? () => signOut() : () => signIn()}
+        >
+          {sessionData ? 'Sign out' : 'Sign in'}
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
