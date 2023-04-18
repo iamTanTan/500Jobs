48f4983d9644b9d9b545061664584707e20e5739
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-04-17 19:19:33 -0700

    update footer size and text colors

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const footer = () => {
   return (
-    <div className='h-10'>
-      <div className='relative flex w-screen flex-col justify-between dark:bg-midnight dark:text-white'>
+    <div className='h-full'>
+      <div className='relative flex w-screen flex-col justify-between bg-primary-50 dark:bg-midnight dark:text-white'>
         <div className='flex justify-between py-6 px-8'>
           <span>
             Made with &hearts; by{' '}
             <a
               href='https://github.com/iamTanTan'
-              className='font-bold text-orange-300 hover:underline'
+              className='font-bold hover:underline'
               target='_blank'
               rel='noreferrer'
             >
@@ -19,7 +19,7 @@ const footer = () => {
           <div className='flex gap-4'>
             <a
               href='https://github.com/iamTanTan'
-              className='font-bold text-orange-300 hover:underline'
+              className='font-bold hover:underline'
               target='_blank'
               rel='noreferrer'
             >
