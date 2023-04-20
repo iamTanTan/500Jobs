86f277705d0026c8a1ceb89359300a0b3ef5a931
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2023-04-20 13:26:30 -0700

    update mobile col width

--- src/components/companyList.tsx
+++ src/components/companyList.tsx
@@ -8,8 +8,8 @@ interface CompanyListProps {
 
 const CompanyList: React.FC<CompanyListProps> = ({ companies }) => {
   return (
-    <div className='grid grid-cols-6 gap-4'>
-      <div className='col-span-4 col-start-2'>
+    <div className='m-2 grid md:grid-cols-6 md:gap-4'>
+      <div className='md:col-span-4 md:col-start-2'>
         <h1 className='my-4 text-center text-3xl font-bold dark:text-white dark:underline'>
           500 List
         </h1>
